314f1d880b4cadad9846554c018eaf03e6e9a41b
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2021-07-07 10:38:04 +0530

    small update still no good css

--- js/main.js
+++ js/main.js
@@ -5,11 +5,24 @@ let no_text=chr(8206)+chr(8207)
 let text=$('input').value
 let limit=$('limit').value
 let sta=0
+
 for(i=0;i<limit;i++){
     sta = (((i+1)*100)/limit).toFixed(3)
-    console.log(sta)
-    $('output').innerHTML=sta
+    $('bar').innerHTML+=sta+'<br>'
     text+=no_text
 }
-$('output').innerHTML=text
+
+
+setTimeout(()=>{alert('complte');$('bar').innerHTML='<br><div style="color:green">complted</div>'},500)
+$('out').innerHTML=`<br>
+<input id='output'></input>
+<br>
+<button onclick="cpy()">COPY</button>`
+$('output').value=text
 }    
+function cpy() {
+    var copyText = $("output");
+    copyText.select();
+    copyText.setSelectionRange(0,$("output").value.length); 
+    document.execCommand("copy");
+}
