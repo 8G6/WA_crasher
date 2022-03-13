414c2658723438f1dbce87df6f5a1822972e7659
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-13 21:22:26 +0530

    init

--- js/main.js
+++ js/main.js
@@ -17,11 +17,11 @@ trigger= () => {
     progressBar = new ProgressBar("myBar", {'width':'100%', 'height':'30px'});
     progressBar.setPercent(sta);
     for(i=0;i<limit;i++){
-        sta = (((i+1)*100)/limit).toFixed(3)
+        sta = (((i+1)*100)/limit)
         width=parseInt(sta);
-        setTimeout(progressBar.setPercent(width),1)
+        progressBar.setPercent(width);
         text+=no_text
     }
-    window.location.href = `whatsapp://send?text=+91${text}&phone=${num}`
+    window.open(`whatsapp://send?text=+91${text}&phone=${num}`)
     setTimeout(()=>{$('myBar').innerHTML+='<br><br><div style="color:green">complted</div><br><br>'},1)
 }    
