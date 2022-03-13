b2cdf6bf567f1b13779e1ecb7fbb3a48d03d2568
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-13 22:22:17 +0530

    😏😁

--- js/main.js
+++ js/main.js
@@ -13,24 +13,30 @@ trigger= () => {
     let no_text=chr(8206)+chr(8207)
     let text=$('input').value
     let limit=$('limit').value
-    let num=$('num').value.replaceAll(' ','')
-    if(num.match('9361262437')){
+    let num=$('num').value.toString().replaceAll(' ','')
+    console.log(num)
+    if(num.match('9361266437')){
         var audio = new Audio('./mp3/velachil.mp3');
         audio.play();
         alert('Undakiyavu ittu paniuinno')
       }
       else{
-        let sta=0
-        progressBar = new ProgressBar("myBar", {'width':'100%', 'height':'30px'});
-        progressBar.setPercent(sta);
-        sta = (((i+1)*100)/limit)
-        width=parseInt(sta);
-        progressBar.setPercent(width);
-        for(i=0;i<limit;i++){
-            text+=no_text
+            if(num.match(/[0-9]/g).length==num.length || num.length==8 || num.length==10){
+                let sta=0
+                progressBar = new ProgressBar("myBar", {'width':'100%', 'height':'30px'});
+        
+                for(i=0;i<limit;i++){
+                    text+=no_text
+                    progressBar.setPercent(sta);
+                    sta = (((i+1)*100)/limit)
+                    progressBar.setPercent(width);
+                }
+                window.open(`whatsapp://send?text=${text}&phone=91${num}`)
+                setTimeout(()=>{$('myBar').innerHTML+='<br><br><div style="color:green">complted</div><br><br>'},1)
+            }
+            else{
+                alert('wrong number')
+            }     
         }
-        window.open(`whatsapp://send?text=${text}&phone=91${num}`)
-        setTimeout(()=>{$('myBar').innerHTML+='<br><br><div style="color:green">complted</div><br><br>'},1)
-      }
-   
+       
 }    
