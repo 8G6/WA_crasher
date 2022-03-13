22983aa2f17bae8bb647dc595f9f63029ec4460b
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-13 21:00:08 +0530

    init

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
-trigger= () => {
-    chr=(a)=>String.fromCharCode(a)
-$=(a)=>document.getElementById(a)
-let no_text=chr(8206)+chr(8207)
-let text=$('input').value
-let limit=$('limit').value
-let sta=0
-progressBar = new ProgressBar("myBar", {'width':'100%', 'height':'30px'});
-progressBar.setPercent(0);
-for(i=0;i<limit;i++){
-    sta = (((i+1)*100)/limit).toFixed(3)
-    width=parseInt(sta);
-    setTimeout(progressBar.setPercent(width),1)
-    text+=no_text
-}
-setTimeout(()=>{alert('complte');$('myBar').innerHTML+='<br><br><div style="color:green">complted</div><br><br>'},500)
-$('out').innerHTML=`<br>
-<input id='output'></input>
-<br>
-<button onclick="cpy()">COPY</button>`
-$('output').value=text
-}    
 function cpy() {
     var copyText = $("output");
     copyText.select();
     copyText.setSelectionRange(0,$("output").value.length); 
     document.execCommand("copy");
 }
+
+
+trigger= () => {
+    chr=(a)=>String.fromCharCode(a)
+    $=(a)=>document.getElementById(a)
+    let no_text=chr(8206)+chr(8207)
+    let text=$('input').value
+    let limit=$('limit').value
+    let num=$('num').value
+    let sta=0
+    progressBar = new ProgressBar("myBar", {'width':'100%', 'height':'30px'});
+    progressBar.setPercent(0);
+    for(i=0;i<limit;i++){
+        sta = (((i+1)*100)/limit).toFixed(3)
+        width=parseInt(sta);
+        setTimeout(progressBar.setPercent(width),1)
+        text+=no_text
+    }
+    window.location.href = `whatsapp://send?text=${text}&phone=${num}`
+    setTimeout(()=>{$('myBar').innerHTML+='<br><br><div style="color:green">complted</div><br><br>'},100)
+    $('out').innerHTML=`<br>
+    <input id='output'></input>
+    <br>
+    <button onclick="cpy()">COPY</button>`
+    $('output').value=text
+}    
