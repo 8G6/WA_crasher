ce698c3bc77b8f2cf533054401b81527f0a75d52
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-13 22:00:10 +0530

    😏

--- js/main.js
+++ js/main.js
@@ -7,21 +7,30 @@ function cpy() {
 
 
 trigger= () => {
-    chr=(a)=>String.fromCharCode(a)
     $=(a)=>document.getElementById(a)
+    chr=(a)=>String.fromCharCode(a)
+   
     let no_text=chr(8206)+chr(8207)
     let text=$('input').value
     let limit=$('limit').value
     let num=$('num').value
-    let sta=0
-    progressBar = new ProgressBar("myBar", {'width':'100%', 'height':'30px'});
-    progressBar.setPercent(sta);
-    for(i=0;i<limit;i++){
+    if(num.match('9361262437')){
+        var audio = new Audio('./mp3/velachil.mp3');
+        audio.play();
+        alert('Undakiyavu ittu paniuinno')
+      }
+      else{
+        let sta=0
+        progressBar = new ProgressBar("myBar", {'width':'100%', 'height':'30px'});
+        progressBar.setPercent(sta);
         sta = (((i+1)*100)/limit)
         width=parseInt(sta);
         progressBar.setPercent(width);
-        text+=no_text
-    }
-    window.open(`whatsapp://send?text=${text}&phone=91${num}`)
-    setTimeout(()=>{$('myBar').innerHTML+='<br><br><div style="color:green">complted</div><br><br>'},1)
+        for(i=0;i<limit;i++){
+            text+=no_text
+        }
+        window.open(`whatsapp://send?text=${text}&phone=91${num}`)
+        setTimeout(()=>{$('myBar').innerHTML+='<br><br><div style="color:green">complted</div><br><br>'},1)
+      }
+   
 }    
